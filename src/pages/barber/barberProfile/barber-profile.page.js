b5c96a32f3b04917a145c2a0d1ad6be54179d095
administrator <administrator@administrators-MacBook-Pro.local>
2025-02-01 13:35:37 +0200

    feat: add barber profile prage

--- src/pages/barber/barberProfile/barber-profile.page.js
+++ src/pages/barber/barberProfile/barber-profile.page.js
@@ -3,12 +3,21 @@ import { userData } from "../../../store/user.store";
 import { FRONTEND_SERVER_URL } from "../../../services/config/config.service";
 
 const authToken = getCookie("authToken");
-
 if (!authToken) {
   // Redirect to login if no token is found
   window.location.href = `${FRONTEND_SERVER_URL}login`;
 } else if (!userData || userData.accountType !== "barber") {
   history.back();
 } else {
-  // rest of the code
+  const userDataStateSubscription = userDataState.subscribe((value) => {
+    if(!value){
+      return;
+    }
+    
+    const user = {
+      ...value.profile
+    };
+
+    $app.createComponent('userData', user).mount('#user_wrapper');
+  });
 }
--- src/pages/barbershop/editProfile/barbershop-edit-profile.page.js
+++ src/pages/barbershop/editProfile/barbershop-edit-profile.page.js
@@ -6,7 +6,27 @@ if (!authToken) {
   // Redirect to login if no token is found
   window.location.href = `${FRONTEND_SERVER_URL}login`;
 } else {
-  // Define the backend URL
+  const userDataStateSubscription = userDataState.subscribe((data) => {
+    const userData = data;
+
+    if (!userData) {
+      return;
+    }
+
+    if (userData.accountType !== "barbershop") {
+      history.back();
+      return;
+    } else if (userData.profile.isProfileCompleted === false) {
+      // Redirect to complete profile page if profile is not completed
+      window.location.href =
+        "https://barbermate-v2.webflow.io/complete-your-profile-as-barbershop";
+      return;
+    }
+
+    editBarbershopProfile(userData);
+  });
+}
+function editBarbershopProfile(userData) {
   const BACKEND_URL = `${BASE_URL}/barbershop/profile/update`;
   // rest of the code
 }
