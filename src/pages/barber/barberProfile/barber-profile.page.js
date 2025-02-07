46471016f1ceafa0a0a6d3c0fabb9b2898f0f92e
Pavel Elenov <pavelelenov@Pavels-MacBook-Pro.local>
2025-02-07 21:30:57 +0200

    feat: add index js for edit barbershop profile

--- src/pages/barber/barberProfile/barber-profile.page.js
+++ src/pages/barber/barberProfile/barber-profile.page.js
@@ -1,23 +1,4 @@
-import { getCookie } from "../../../services/cookie/cookie.service";
 import { userData } from "../../../store/user.store";
-import { FRONTEND_SERVER_URL } from "../../../services/config/config.service";
 
-const authToken = getCookie("authToken");
-if (!authToken) {
-  // Redirect to login if no token is found
-  window.location.href = `${FRONTEND_SERVER_URL}login`;
-} else if (!userData || userData.accountType !== "barber") {
-  history.back();
-} else {
-  const userDataStateSubscription = userDataState.subscribe((value) => {
-    if(!value){
-      return;
-    }
-    
-    const user = {
-      ...value.profile
-    };
-
-    $app.createComponent('userData', user).mount('#user_wrapper');
-  });
-}
+const user = userData.profile;
+$app.createComponent("userData", user).mount("#user_wrapper");
---
+++ src/pages/barbershop/editProfile/index.js
@@ -0,0 +1,3 @@
+export * from "./barbershop-edit-profile.page";
+export * from "./validations/barbershop-edit-profile-form-validations.page";
+export * from "../completeProfile/validations/fileValidation/barbershop-complete-profile-file.validations";
